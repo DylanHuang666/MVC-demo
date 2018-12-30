afc78264bd896aca36f9ab5c529272890050366a
Dylan <huanglinjay123@outlook.com>
2018-12-30 19:17:22 +0800

    引入Vue

--- main.js
+++ main.js
@@ -18,19 +18,6 @@ Model.prototype.update=function(id,data){        //Model类共有方法
     })
 }
 
-//----------------构造函数封装View类
-function View({el,template}){
-    this.el=el
-    this.template=template
-}
-View.prototype.render=function(data){          //View类共有方法
-    let html=this.template
-    for(let key in data){
-        html=html.replace(`__${key}__`,data[key])     
-    }
-    $(this.el).html(html)
-}
-
 
 //-------------------声明model,view对象
 let model=new Model({
@@ -42,71 +29,62 @@ let model=new Model({
     resouce:'book'
 })
 
-let view=new View({
-    el:'#app',
+let view=new Vue({         //new Vue()
+    el:'#app',       //template只能有一个根元素，所有再用一个div包住
+    data:{
+        book:{
+            name:'未命名',
+            number:0,
+            id:''
+        },
+        n:1
+    },           
     template:`
-    <div>
-        书名：《__name__》
-        数量：<span id="number">__number__</span>
+    <div>                       
+        <div>
+            书名：《{{book.name}}》
+            数量：<span id="number">{{book.number}}</span>
+        </div>
+        <div>
+            <input v-model="n"/>
+            n的值为{{n}} 
+        </div>
+        <div>
+            <button v-on:click="addOne">加n</button>
+            <button v-on:click="minusOne">减n</button>
+            <button v-on:click="reset">归零</button>
+        </div>
     </div>
-    <div>
-        <button id="addOne">加一</button>
-        <button id="minusOne">减一</button>
-        <button id="reset">归零</button>
-    </div>
-`})
-
-
-//controller模块
-var controller={           //其他逻辑执行代码
-    init(options){
-        let{view,model}=options
-        this.view=view
-        this.model=model
-        this.bindEvents()
-        this.model.fetch(1)
-            .then(()=>{
-                this.view.render(this.model.data)  
+    `,
+    created(){
+        model.fetch(1).then(()=>{       //初始化完后就执行这个函数created
+            this.book=model.data
+        })
+    },
+    methods:{
+        addOne(){
+            model.update(1,{
+                number:this.book.number+(this.n-0)
+            }).then(()=>{
+                this.book=model.data
             })
-        this.view.render(this.model.data)
-        
-    },
-    addOne(){
-        var oldNumber=$('#number').text()
-        var newNumber=oldNumber-0+1
-        this.model.update(1,{
-            number:newNumber
-        }).then(()=>{
-            this.view.render(this.model.data)
-        })
-    },
-    minusOne(){
-        var oldNumber=$('#number').text()
-        var newNumber=oldNumber-0-1
-        this.model.update(1,{
-            number:newNumber
-        }).then(()=>{
-            this.view.render(this.model.data)
-        })
-    },
-    reset(){
-        this.model.update(1,{
-            number:0
-        }).then(()=>{
-            this.view.render(this.model.data)
-        })
-    },
-    bindEvents(){
-        $(this.view.el).on('click','#addOne',this.addOne.bind(this))        //bind(this)绑定this
-        $(this.view.el).on('click','#minusOne',this.minusOne.bind(this))
-        $(this.view.el).on('click','#reset',this.reset.bind(this))
+        },
+        minusOne(){
+            model.update(1,{
+                number:this.book.number-(this.n-0)
+            }).then(()=>{
+                this.book=model.data
+            })
+        },
+        reset(){
+            model.update(1,{
+                number:0
+            }).then(()=>{
+                this.book=model.data
+            })
+        }
     }
-}
-
-
-controller.init({view:view,model:model})   //执行
-
-
+})
 
 
 //造一个假的数据后台
